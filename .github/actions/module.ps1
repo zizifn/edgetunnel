0fd00298b2f0de47e5973b27c9ab58025c4fdbdd
zizifn <1803942+zizifn@users.noreply.github.com>
2020-12-13 20:03:40 +0800

    chore: update

--- .github/actions/module.ps1
+++ .github/actions/module.ps1
@@ -1,3 +1,11 @@
 function Show-Calendar {
     Write-Host "output - Show-Calendar"
 }
+
+function Show-Calendar2 {
+    Write-Host "output - Show-Calendar2"
+}
+
+function Show-Calendar3 {
+    Write-Host "output - Show-Calendar3"
+}
