1460c8fdfec6ea82ca62f9ef8fad76170b1278f5
zizifn <1803942+zizifn@users.noreply.github.com>
2020-12-13 21:04:31 +0800

    chore: update

---
+++ v2rayconf.sh
@@ -0,0 +1,27 @@
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
