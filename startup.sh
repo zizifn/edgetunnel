a6ae9fd60739b1348ee9cd0c8af3ad63c0b6a7d0
zizifn <1803942+zizifn@users.noreply.github.com>
2020-12-31 22:14:43 +0800

    chore: add policy

--- startup.sh
+++ startup.sh
@@ -2,6 +2,25 @@
 
 cat << EOF > /etc/v2ray/config.json
 {
+    "policy": {
+        "levels": {
+            "0": {
+                "handshake": 5,
+                "connIdle": 300,
+                "uplinkOnly": 2,
+                "downlinkOnly": 5,
+                "statsUserUplink": false,
+                "statsUserDownlink": false,
+                "bufferSize": 10240
+            }
+        },
+        "system": {
+            "statsInboundUplink": false,
+            "statsInboundDownlink": false,
+            "statsOutboundUplink": false,
+            "statsOutboundDownlink": false
+        }
+    },
     "inbounds": [
         {
             "port": $PORT,
