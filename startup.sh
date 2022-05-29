05689e3ed34dc6892d9b708948bb32c694ff408d
zizifn <515868058@qq.com>
2022-05-29 14:14:59 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -18,7 +18,8 @@ echo 'has tunnel token, run cloudflared tunnel'
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
 chmod +x /root/cloudflared
 # /usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
-/usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & caddy start --config /root/Caddyfile
+caddy start --config /root/Caddyfile
+/usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN
 else
 caddy start --config /root/Caddyfile
 /usr/bin/v2ray -config /root/config.json
