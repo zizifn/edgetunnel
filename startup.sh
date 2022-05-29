86b0d4b5c5062c48f595044786b69b903d03f4db
zizifn <515868058@qq.com>
2022-05-29 15:29:33 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,7 +20,6 @@ chmod +x /root/cloudflared
 # /usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
 /usr/bin/v2ray -config /root/config.json & caddy run --config /root/Caddyfile & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol http2
 else
-caddy start --config /root/Caddyfile
-/usr/bin/v2ray -config /root/config.json
+/usr/bin/v2ray -config /root/config.json & caddy run --config /root/Caddyfile
 fi
 
