fbbfe2d08f0e3499f6cc29b6ff814581ea4419af
yaolin <yaolinforward@qq.com>
2022-11-21 16:50:59 +0000

    modify v2ray command for v5.1.0

--- startup.sh
+++ startup.sh
@@ -15,13 +15,16 @@ else
 fi
 
 # Run V2Ray
+echo "Start V2Ray ..."
 if [[ $TUNNEL_TOKEN ]]; then
 echo 'has tunnel token, run cloudflared tunnel'
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
 chmod +x /root/cloudflared
 # /usr/bin/v2ray -config /root/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
-v2ray -config /root/config.json & caddy run --config /root/Caddyfile & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol http2
+echo "Start V2Ray and cloudflared tunnel"
+v2ray run -c /root/config.json & caddy run --config /root/Caddyfile & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol http2
 else
-v2ray -config /root/config.json & caddy run --config /root/Caddyfile
+echo "Start V2Ray without cloudflared"
+v2ray run -c /root/config.json & caddy run --config /root/Caddyfile
 fi
 
