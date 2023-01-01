31924d8a837876f284a68155167d2255e7c92324
zizifn <1803942+zizifn@users.noreply.github.com>
2023-01-02 04:03:26 +0800

    add cf page

--- apps/cf-page/src/main.tsx
+++ apps/cf-page/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import App from './app/app';
+// import App from './app/app';
+import { EdgeApp } from 'edge-ui';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <EdgeApp />
   </StrictMode>
 );
