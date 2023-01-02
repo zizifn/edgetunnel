fde32ba4eae3b59ff7bf45d0c925ae4f902709c8
zizifn <515868058@qq.com>
2023-01-02 22:51:27 +0800

    refactor vless

--- functions/ws.ts
+++ functions/ws.ts
@@ -3,16 +3,16 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async ({ request }) => {
-  const upgradeHeader = request.headers.get("Upgrade");
-  if (!upgradeHeader || upgradeHeader !== "websocket") {
-    return new Response("Expected Upgrade: websocket", { status: 426 });
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
   }
 
   const webSocketPair = new WebSocketPair();
   const [client, server] = Object.values(webSocketPair);
 
   server.accept();
-  server.addEventListener("message", (event) => {
+  server.addEventListener('message', (event) => {
     console.log(event.data);
     server.send(`server reponse after client sent ${event.data}`);
   });
--- test.mjs
+++ test.mjs
@@ -1,4 +1,34 @@
-import { Readable } from 'stream';
-const readableStream = new Readable();
-readableStream.push('ping!');
-readableStream.push('pong!');
+import { ReadableStream } from 'stream/web';
+
+try {
+  const readableStream = new ReadableStream({
+    start(control) {
+      setInterval(() => {
+        control.enqueue('11');
+      }, 1000);
+    },
+    pull(control) {
+      control.enqueue('11');
+      // undefined.length;
+      // control.close();
+      //   control.error('error');
+      //   undefined.length;
+    },
+  });
+
+  await readableStream.pipeTo(
+    new WritableStream({
+      write(chunk, controller) {
+        console.log(chunk);
+      },
+    })
+  );
+
+  console.log('end--------');
+
+  //   for await (const iterator of readableStream) {
+  //     console.log(iterator);
+  //   }
+} catch (error) {
+  console.log('---end---', error);
+}
