d99a4f80e7137c0b7c196ed5ecf87b49dfbe4612
zizifn <515868058@qq.com>
2023-02-25 13:56:31 +0800

    upgrade nx version

--- functions/http2.ts
+++ functions/http2.ts
@@ -3,13 +3,21 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+  let count = 0;
+  console.log('test11', Date.now());
+  await new Promise((resolve, rej) => {
+    setTimeout(() => {
+      resolve('');
+    }, 100);
+  });
+  console.log('test11', Date.now());
   const transformStream =
     context.request.body?.pipeThrough(new TextDecoderStream()).pipeThrough(
       new TransformStream({
         transform(chunk, controller) {
-          console.log('test');
+          console.log('test', Date.now());
           controller.enqueue(
-            new TextEncoder().encode(`${chunk} +  ${new Date()}`)
+            new TextEncoder().encode(`${chunk} + ${count++}  ${new Date()}`)
           );
         },
       })
