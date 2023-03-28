14adbb26ceabe05b12ed277c3cd32fc4fbb7c582
zizifn <515868058@qq.com>
2023-03-28 21:42:41 +0800

    refactor deno

--- apps/deno-vless/src/client.ts
+++ apps/deno-vless/src/client.ts
@@ -3,17 +3,27 @@ import {
   serveFile,
 } from 'https://deno.land/std@0.167.0/http/file_server.ts';
 async function serveClient(req: Request, basePath: string) {
-  const pathname = new URL(req.url).pathname;
-  if (pathname.startsWith('/assets')) {
-    const resp = await serveDir(req, {
-      fsRoot: `${Deno.cwd()}/dist/apps/cf-page`,
-    });
-    resp.headers.set('cache-control', 'public, max-age=2592000');
+  const url = new URL(req.url)
+  if (url.pathname.startsWith('/assets')) {
+    // const resp = await serveDir(req, {
+    //   fsRoot: `${Deno.cwd()}/dist/apps/cf-page`,
+    // });
+    // resp.headers.set('cache-control', 'public, max-age=2592000');
+    const resp = await fetch(`https://raw.githubusercontent.com/zizifn/edgetunnel/main/dist/apps/cf-page${url.hostname}`)
+    if(url.hostname.endsWith('.js')){
+      resp.headers.set('content-type', 'application/javascript');
+    }
+    if(url.hostname.endsWith('.css')){
+      resp.headers.set('content-type', 'text/css');
+    }
+    if(url.pathname.includes(basePath)){
+      resp.headers.set('content-type', 'text/html; charset=utf-8');
+    }
     return resp;
   }
-  if (pathname.includes(basePath)) {
-    return await serveFile(req, `${Deno.cwd()}/dist/apps/cf-page/index.html`);
-  }
+  // if (url.pathname.includes(basePath)) {
+  //   return await serveFile(req, `${Deno.cwd()}/dist/apps/cf-page/index.html`);
+  // }
   const basicAuth = req.headers.get('Authorization') || '';
   const authString = basicAuth.split(' ')?.[1] || '';
   if (atob(authString).includes(basePath)) {
