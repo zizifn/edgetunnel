8e45d976699c3f557c403ed2cfe9845be8ab6e9e
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-03-28 11:54:51 +0700

    update files

--- apps/deno-playground/index.ts
+++ apps/deno-playground/index.ts
@@ -1,8 +1,14 @@
 // 导入相关模块和库
 import { serve } from 'https://deno.land/std@0.170.0/http/server.ts';
 import * as uuid from 'https://jspm.dev/uuid';
-import * as lodash from 'https://jspm.dev/lodash-es';
-
+// import * as lodash from 'https://jspm.dev/lodash-es';
+import {
+    closeWebSocket,
+    // delay,
+    makeReadableWebSocketStream,
+    processVlessHeader,
+  } from 'https://raw.githubusercontent.com/3Kmfi6HP/edgetunnel/main/apps/deno-playground/vless-js.ts';
+ 
 // 获取 UUID 环境变量或者默认值
 const userID = Deno.env.get('UUID') || '41446577-9ca4-4358-8082-1be0e65aace0';
 // 多用户模式
@@ -42,7 +48,6 @@ async function serveClient(req: Request, basePath: string) {
         const env = Deno.env.toObject();
         const responseObj = {
             message: 'hello world',
-            supabaseUrl: Deno.env.get('SUPABASE_URL') || '',
             upgradeHeader: req.headers.get('upgrade') || '',
             authString: authString,
             uuidValidate: basePath,
@@ -124,328 +129,9 @@ export {
     serveClient
 };
 
-function uuidValidate(this: void, value: string, index: number, obj: string[]): value is any {
-    throw new Error('Function not implemented.');
-}
-
-export function vlessJs(): string {
-    return 'vless-js';
-}
-/*
-定义 delay 函数，返回一个 Promise 对象，延迟指定的毫秒数
-参数：ms - 毫秒数
-*/
-export function delay(ms: number) {
-    return new Promise((resolve, rej) => {
-        setTimeout(resolve, ms);
-    });
-}
-/*
-
-定义 processWebSocket 函数，返回一个 Promise 对象，用于处理 WebSocket
-参数：一个包含 userID、webSocket、rawTCPFactory、libs 等属性的对象
-userID - 用户 ID
-webSocket - WebSocket 对象
-rawTCPFactory - 创建一个 TCP 连接的函数，返回一个 Promise 对象
-libs - 包含 uuid 和 lodash 属性的对象
-*/
-export async function processWebSocket({
-    userID,
-    webSocket,
-    rawTCPFactory,
-    libs: {
-        uuid,
-        lodash
-    },
-}: {
-    userID: string;
-    webSocket: WebSocket;
-    rawTCPFactory: (port: number, hostname: string) => Promise < any > ;
-    libs: {
-        uuid: any;lodash: any
-    };
-}) {
-    let address = ''; // 远程服务器地址
-    let portWithRandomLog = ''; // 远程服务器端口号
-    let remoteConnection: { // 远程 TCP 连接
-        readable: any;
-        writable: any;
-        write: (arg0: Uint8Array) => any;
-        close: () => void;
-    } | null = null; // 远程 TCP 连接对象，初始值为 null
-    let remoteConnectionReadyResolve: Function; // remoteConnectionPromise 的解决函数
-    try {
-        const log = (info: string, event ? : any) => {
-        };
-        const readableWebSocketStream = makeReadableWebSocketStream(webSocket, log); // 创建可读的 WebSocket 流
-        let vlessResponseHeader: Uint8Array | null = null;// VLESS 协议响应头
-        // ws --> remote
-        // 将可读的 WebSocket 流中的数据发送到远程 TCP 连接中
-        readableWebSocketStream
-            .pipeTo(
-                new WritableStream({
-                    async write(chunk, controller) {
-                        const vlessBuffer = chunk; // 读取 WebSocket 数据流中的数据
-                        if (remoteConnection) {
-                            const number = await remoteConnection.write(
-                                new Uint8Array(vlessBuffer)
-                            );
-                            return;
-                        }// 处理 VLESS 协议头部
-                        const {
-                            hasError,
-                            message,
-                            portRemote,
-                            addressRemote,
-                            rawDataIndex,
-                            vlessVersion,
-                        } = processVlessHeader(vlessBuffer, userID, uuid, lodash);
-                        address = addressRemote || ''; // 获取远程服务器地址
-                        portWithRandomLog = `${portRemote}--${Math.random()}`;
-                        if (hasError) {
-                            controller.error(`[${address}:${portWithRandomLog}] ${message} `);
-                        }// 连接远程 TCP 服务器
-                        remoteConnection = await rawTCPFactory(portRemote!, address!);
-                        vlessResponseHeader = new Uint8Array([vlessVersion![0], 0]);// 构造 VLESS 协议响应头
-                        const rawClientData = vlessBuffer.slice(rawDataIndex!);
-                        // 将客户端发来的数据（不包括 VLESS 协议头部）发送到远程 TCP 服务器
-                        await remoteConnection!.write(new Uint8Array(rawClientData));
-                        // 远程连接已准备好，调用 remoteConnectionReadyResolve 函数
-                        remoteConnectionReadyResolve(remoteConnection);
-                    },
-                    close() {
-                    },
-                    abort(reason) {
-                    },
-                })
-            )
-            .catch((error) => {
-            });
-        // 等待远程连接准备好
-        await new Promise((resolve) => (remoteConnectionReadyResolve = resolve));
-        let remoteChunkCount = 0;
-        let totoal = 0;
-        // 将远程 TCP 服务器的数据流发送回客户端的 WebSocket 连接中
-        // remote --> ws
-        await remoteConnection!.readable.pipeTo(
-            new WritableStream({
-                start() {
-                    if (webSocket.readyState === webSocket.OPEN) {
-                        webSocket.send(vlessResponseHeader!);
-                    }
-                },
-                async write(chunk: Uint8Array, controller) {
-                    function send2WebSocket() {
-                        if (webSocket.readyState !== webSocket.OPEN) {
-                            controller.error(
-                                `can't accept data from remoteConnection!.readable when client webSocket is close early`
-                            );
-                            return;
-                        }
-                        webSocket.send(chunk);
-                    }
-
-                    remoteChunkCount++;
-                    send2WebSocket();
-                },
-                close() {
-                },
-                abort(reason) {
-                    closeWebSocket(webSocket);
-                },
-            })
-        );
-    } catch (error: any) {
-        closeWebSocket(webSocket);
-    }
-    return;
-}
-// 从客户端的 WebSocket 连接中读取数据并发送到远程 TCP 服务器
-// ws --> remote
-// 返回值：ReadableStream 对象
-export function makeReadableWebSocketStream(
-    ws: WebSocket | any,
-    log: Function
-) {
-    let readableStreamCancel = false;
-    return new ReadableStream < ArrayBuffer > ({
-        // 开始读取数据
-        start(controller) {
-            ws.addEventListener('message', async (e: {
-                data: ArrayBuffer
-            }) => {
-                const vlessBuffer: ArrayBuffer = e.data;
-
-                controller.enqueue(vlessBuffer);
-            });
-            ws.addEventListener('error', (e: any) => {
-                log('socket has error');
-                readableStreamCancel = true;
-                controller.error(e);
-            });
-            ws.addEventListener('close', () => {
-                try {
-                    log('webSocket is close');
-                    if (readableStreamCancel) {
-                        return;
-                    }
-                    controller.close();
-                } catch (error) {
-                    log(`websocketStream can't close DUE to `, error);
-                }
-            });
-        },
-        // 拉取数据（本函数中为空函数）
-        pull(controller) {},
-        // 取消数据读取
-        cancel(reason) {
-            log(`websocketStream is cancel DUE to `, reason);
-            if (readableStreamCancel) {
-                return;
-            }
-            readableStreamCancel = true;
-            closeWebSocket(ws);
-        },
-    });
-}
-// 关闭 WebSocket 连接
-// 参数：WebSocket 对象
-export function closeWebSocket(socket: WebSocket | any) {
-    if (socket.readyState === socket.OPEN) {
-        socket.close();
-    }
-}
-
-/*
-函数名称：processVlessHeader
-函数描述：处理VLESS协议头部信息
-参数：
-vlessBuffer: ArrayBuffer类型，VLESS协议头部数据
-userID: string类型，用户ID
-uuidLib: any类型，uuid库
-lodash: any类型，lodash库
-返回值：
-Object类型，包含以下属性：
-hasError: boolean类型，表示是否存在错误
-message: string类型，存在错误时的错误信息
-addressRemote: string类型，远程地址
-portRemote: number类型，远程端口
-rawDataIndex: number类型，原始数据索引
-vlessVersion: Uint8Array类型，VLESS协议版本
-*/
-export function processVlessHeader(
-    vlessBuffer: ArrayBuffer,
-    userID: string,
-    uuidLib: any,
-    lodash: any
-) {
-    if (vlessBuffer.byteLength < 24) {
-        return {
-            hasError: true,
-            message: 'invalid data',
-        };
-    }
-    const version = new Uint8Array(vlessBuffer.slice(0, 1));
-    let isValidUser = false;
-    if (uuidLib.stringify(new Uint8Array(vlessBuffer.slice(1, 17))) === userID) {
-        isValidUser = true;
-    }
-    if (!isValidUser) {
-        return {
-            hasError: true,
-            message: 'in valid user',
-        };
-    }
-
-    const optLength = new Uint8Array(vlessBuffer.slice(17, 18))[0];
-
-    const command = new Uint8Array(
-        vlessBuffer.slice(18 + optLength, 18 + optLength + 1)
-    )[0];
-    if (command === 1) {} else {
-        return {
-            hasError: true,
-            message: `command ${command} is not support, command 01-tcp,02-udp,03-mux`,
-        };
-    }
-    const portIndex = 18 + optLength + 1;
-    const portBuffer = vlessBuffer.slice(portIndex, portIndex + 2);
-    const portRemote = new DataView(portBuffer).getInt16(0);
-
-    let addressIndex = portIndex + 2;
-    const addressBuffer = new Uint8Array(
-        vlessBuffer.slice(addressIndex, addressIndex + 1)
-    );
-
-    const addressType = addressBuffer[0];
-    let addressLength = 0;
-    let addressValueIndex = addressIndex + 1;
-    let addressValue = '';
-    switch (addressType) {
-        case 1:
-            // 如果地址类型是 1，即 IPv4 地址
-            addressLength = 4;
-            // 地址长度为 4
-            addressValue = new Uint8Array(
-                vlessBuffer.slice(addressValueIndex, addressValueIndex + addressLength)
-            ).join('.'); // 取出地址值并用点号拼接
-            break;
-        case 2:
-            // 如果地址类型是 2，即域名
-            addressLength = new Uint8Array(
-                vlessBuffer.slice(addressValueIndex, addressValueIndex + 1)
-            )[0];
-            // 取出域名长度
-            addressValueIndex += 1;
-            // 更新地址值索引
-            addressValue = new TextDecoder().decode(
-                vlessBuffer.slice(addressValueIndex, addressValueIndex + addressLength)
-            );
-            // 取出域名并解码
-            break;
-        case 3:
-            // 如果地址类型是 3，即 IPv6 地址
-            addressLength = 16;
-            // 地址长度为 16
-            const addressChunkBy2: number[][] = lodash.chunk(
-                new Uint8Array(
-                    vlessBuffer.slice(
-                        addressValueIndex,
-                        addressValueIndex + addressLength
-                    )
-                ),
-                2,
-                null
-            );
-            // 将地址值按每 2 个字节分割为二维数组
-            addressValue = addressChunkBy2
-                .map((items) =>
-                    items.map((item) => item.toString(16).padStart(2, '0')).join('')
-                )
-                .join(':'); // 将二维数组转为 IPv6 地址字符串
-            if (addressValue) {
-                addressValue = `[${addressValue}]`;
-            }// 如果地址值非空，将其括在方括号中
-            break;
-        default:
-    }
-    if (!addressValue) {
-        // 如果地址值为空
-        return {
-            hasError: true,
-            message: `addressValue is empty, addressType is ${addressType}`,
-        };// 返回错误信息
-    }
-    // 如果地址值非空，继续执行
-    return {
-        hasError: false,
-        addressRemote: addressValue,
-        portRemote,
-        rawDataIndex: addressValueIndex + addressLength,
-        vlessVersion: version,
-    };
-}
-// 返回解析出的地址和端口信息
+// function uuidValidate(this: void, value: string, index: number, obj: string[]): value is any {
+//     throw new Error('Function not implemented.');
+// } 
 let isVaildUser = uuid.validate(userID);
 // 验证用户 ID 是否有效
 if (!isVaildUser) {
@@ -480,35 +166,202 @@ const handler = async (req: Request): Promise < Response > => {
         response // WebSocket 响应
     } = Deno.upgradeWebSocket(req); // 升级 HTTP 连接为 WebSocket 连接
     socket.addEventListener('open', () => {}); // 监听 WebSocket 打开事件
+// let test: Deno.TcpConn | null = null;
+  // test!.writable.abort();
+  //
+  const earlyDataHeader = req.headers.get('sec-websocket-protocol') || '';
 
-    processWebSocket({ // 处理 WebSocket 连接
-        userID,
-        webSocket: socket,
-        rawTCPFactory: (port: number, hostname: string) => { // 创建 TCP 连接
-            return Deno.connect({
-                port,
-                hostname,
-            });
-        },
-        libs: { // 依赖库
-            uuid,
-            lodash
-        },
-    });
-    return response; // 返回 WebSocket 响应
+  processWebSocket({
+    userID,
+    webSocket: socket,
+    earlyDataHeader,
+    // rawTCPFactory: (port: number, hostname: string) => {
+    //   return Deno.connect({
+    //     port,
+    //     hostname,
+    //   });
+    // },
+  });
+  return response;
 };
 
-// 在页面即将卸载和已经卸载时输出日志
+async function processWebSocket({
+  userID,
+  webSocket,
+  earlyDataHeader,
+}: // libs: { uuid, lodash },
+{
+  userID: string;
+  webSocket: WebSocket;
+  earlyDataHeader: string;
+  // rawTCPFactory: (port: number, hostname: string) => Promise<any>;
+  // libs: { uuid: any; lodash: any };
+}) {
+  let address = '';
+  let portWithRandomLog = '';
+  let remoteConnection: {
+    readable: any;
+    writable: any;
+    write: (arg0: Uint8Array) => any;
+    close: () => void;
+  } | null = null;
+  let remoteConnectionReadyResolve: Function;
+  try {
+    const log = (info: string, event?: any) => {
+      console.log(`[${address}:${portWithRandomLog}] ${info}`, event || '');
+    };
+    const readableWebSocketStream = makeReadableWebSocketStream(
+      webSocket,
+      earlyDataHeader,
+      log
+    );
+    let vlessResponseHeader: Uint8Array | null = null;
+
+    // ws --> remote
+    readableWebSocketStream
+      .pipeTo(
+        new WritableStream({
+          async write(chunk, controller) {
+            const vlessBuffer = chunk;
+            if (remoteConnection) {
+              const number = await remoteConnection.write(
+                new Uint8Array(vlessBuffer)
+              );
+              return;
+            }
+            const {
+              hasError,
+              message,
+              portRemote,
+              addressRemote,
+              rawDataIndex,
+              vlessVersion,
+              isUDP,
+            } = processVlessHeader(vlessBuffer, userID);
+            address = addressRemote || '';
+            portWithRandomLog = `${portRemote}--${Math.random()}`;
+            if (isUDP) {
+              console.log('udp');
+              controller.error(
+                `[${address}:${portWithRandomLog}] command udp is not support `
+              );
+              return;
+            }
+            if (hasError) {
+              controller.error(`[${address}:${portWithRandomLog}] ${message} `);
+            }
+            // const addressType = requestAddr >> 4;
+            // const addressLength = requestAddr & 0x0f;
+            console.log(`[${address}:${portWithRandomLog}] connecting`);
+            remoteConnection = await Deno.connect({
+              port: portRemote!,
+              hostname: address,
+            });
+            vlessResponseHeader = new Uint8Array([vlessVersion![0], 0]);
+            const rawClientData = vlessBuffer.slice(rawDataIndex!);
+            await remoteConnection!.write(new Uint8Array(rawClientData));
+            remoteConnectionReadyResolve(remoteConnection);
+          },
+          close() {
+            console.log(
+              `[${address}:${portWithRandomLog}] readableWebSocketStream is close`
+            );
+          },
+          abort(reason) {
+            console.log(
+              `[${address}:${portWithRandomLog}] readableWebSocketStream is abort`,
+              JSON.stringify(reason)
+            );
+          },
+        })
+      )
+      .catch((error) => {
+        console.error(
+          `[${address}:${portWithRandomLog}] readableWebSocketStream pipeto has exception`,
+          error.stack || error
+        );
+        // error is cancel readable stream anyway, no need close websocket in here
+        // closeWebSocket(webSocket);
+        // close remote conn
+        // remoteConnection?.close();
+      });
+    await new Promise((resolve) => (remoteConnectionReadyResolve = resolve));
+    let remoteChunkCount = 0;
+    let totoal = 0;
+    // remote --> ws
+    await remoteConnection!.readable.pipeTo(
+      new WritableStream({
+        start() {
+          if (webSocket.readyState === webSocket.OPEN) {
+            webSocket.send(vlessResponseHeader!);
+          }
+        },
+        async write(chunk: Uint8Array, controller) {
+          function send2WebSocket() {
+            if (webSocket.readyState !== webSocket.OPEN) {
+              controller.error(
+                `can't accept data from remoteConnection!.readable when client webSocket is close early`
+              );
+              return;
+            }
+            webSocket.send(chunk);
+          }
+
+          remoteChunkCount++;
+          //#region
+          // console.log(
+          //   `${(totoal +=
+          //     chunk.length)}, count: ${remoteChunkCount.toString()}, ${
+          //     chunk.length
+          //   }`
+          // );
+          // https://github.com/zizifn/edgetunnel/issues/87, hack for this issue, maybe websocket sent too many small chunk,
+          // casue v2ray client can't process https://github.com/denoland/deno/issues/17332
+          // limit X number count / bandwith, due to deno can't read bufferedAmount in deno,
+          // this is deno bug and this will not need in nodejs version
+          //#endregion
+        //   if (remoteChunkCount < 20) {
+        //     send2WebSocket();
+        //   } else if (remoteChunkCount < 120) {
+        //     await delay(10); // 64kb * 100 = 6m/s
+        //     send2WebSocket();
+        //   } else if (remoteChunkCount < 500) {
+        //     await delay(20); // (64kb * 1000/20) = 3m/s
+        //     send2WebSocket();
+        //   } else {
+        //     await delay(50); // (64kb * 1000/50)  /s
+            send2WebSocket();
+        //   }
+        },
+        close() {
+          console.log(
+            `[${address}:${portWithRandomLog}] remoteConnection!.readable is close`
+          );
+        },
+        abort(reason) {
+          closeWebSocket(webSocket);
+          console.error(
+            `[${address}:${portWithRandomLog}] remoteConnection!.readable abort`,
+            reason
+          );
+        },
+      })
+    );
+  } catch (error: any) {
+    console.error(
+      `[${address}:${portWithRandomLog}] processWebSocket has exception `,
+      error.stack || error
+    );
+    closeWebSocket(webSocket);
+  }
+  return;
+}
+
 globalThis.addEventListener('beforeunload', (e) => {
-    console.log('About to exit...');
+  console.log('About to exit...');
 });
 
 globalThis.addEventListener('unload', (e) => {
-    console.log('Exiting');
+  console.log('Exiting');
 });
-
-// 启动服务器
-serve(handler, {
-    port: 8080,
-    hostname: '0.0.0.0'
-});
+serve(handler, { port: 8080, hostname: '0.0.0.0' });
