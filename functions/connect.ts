ff4885e86573d4aa5b70740b3c1654405d67f209
zizifn3 <75520940+zizifn3@users.noreply.github.com>
2023-05-16 14:10:43 +0000

    add tcp test

---
+++ functions/connect.ts
@@ -0,0 +1,34 @@
+// ignore the typescropt error
+// @ts-ignore
+import { connect } from 'cloudflare:sockets';
+
+export default {
+  async fetch(req, env) {
+    console.log('start fetch');
+    const socket = connect({
+      hostname: 'neverssl.com',
+      port: 80,
+    });
+
+    const writer = socket.writable.getWriter();
+    const encoder = new TextEncoder();
+    const encoded = encoder.encode(
+      'GET / HTTP/1.1\r\nHost: neverssl.com\r\n\r\n'
+    );
+    await writer.write(encoded);
+
+    const reader = socket.readable.getReader();
+    const decoder = new TextDecoder();
+    let response = '';
+    while (true) {
+      const res = await reader.read();
+      if (res.done) {
+        console.log('Stream done, socket connection has been closed.');
+        break;
+      }
+      response += decoder.decode(res.value);
+    }
+
+    return new Response(response);
+  },
+};
