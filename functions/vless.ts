8ee741e73ddf71a1f5f65e94ddf60bba8d80bd68
zizifn <1803942+zizifn@users.noreply.github.com>
2023-05-19 00:58:53 +0800

    change vless to 401 for http

--- functions/vless.ts
+++ functions/vless.ts
@@ -4,6 +4,7 @@ import {
   vlessJs,
 } from 'vless-js';
 import { connect } from 'cloudflare:sockets';
+import { page404 } from './util';
 
 interface Env {
   KV: KVNamespace;
@@ -21,8 +22,12 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 
   const upgradeHeader = context.request.headers.get('Upgrade');
   if (!upgradeHeader || upgradeHeader !== 'websocket') {
-    return new Response(`Expected Upgrade: websocket`, {
-      status: 404,
+    return new Response(``, {
+      status: 401,
+      headers: {
+        'content-type': 'text/html; charset=utf-8',
+        'WWW-Authenticate': 'Basic',
+      },
     });
   }
 
