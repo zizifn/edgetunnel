d69aad56e58253a9c68a17221c65dce181468bbc
rikkagcp1 <rikkagcp1>
2023-12-03 22:17:20 +1100

    Add deno support, improve node support

--- node/index.js
+++ node/index.js
@@ -3,11 +3,12 @@
 // To run, first run "setup.sh", then "node index.js"
 
 import http from 'http';
-import net from 'net';
 import WebSocket from 'ws';
-import {createSocket as createUDPSocket} from 'dgram';
 
-import {globalConfig, platformAPI, setConfigFromEnv, vlessOverWSHandler, getVLESSConfig} from '../src/worker-with-socks5-experimental.js';
+import {globalConfig, setConfigFromEnv, vlessOverWSHandler, getVLESSConfig} from '../src/worker-neo.js';
+import {onNodeStart} from './nodeplatform.js';
+
+onNodeStart();
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
@@ -32,116 +33,13 @@ wss.on('connection', (ws, req) => {
 	vlessOverWSHandler(ws, req.headers['sec-websocket-protocol'] || '');
 });
 
-// Start the server on port 8080
-server.listen(8080);
+// Start the server on port 8000
+server.listen(8000);
 
 function buf2hex(buffer) { // buffer is an ArrayBuffer
 	return [...new Uint8Array(buffer)]
 		.map(x => x.toString(16).padStart(2, '0'))
 		.join(' ');
-}
-
-/**
- * Portable function for creating a outbound TCP connections.
- * Has to be "async" because some platforms open TCP connection asynchronously.
- * 
- * @param {string} address The remote address to connect to.
- * @param {number} port The remote port to connect to.
- * @returns {object} The wrapped TCP connection, to be compatible with Cloudflare Workers
- */
-platformAPI.connect = async (address, port) => {
-	const socket = net.createConnection(port, address);
-
-	let readableStreamCancel = false;
-	const readableStream = new ReadableStream({
-		start(controller) {
-			socket.on('data', (data) => {
-				if (readableStreamCancel) {
-					return;
-				}
-				controller.enqueue(data);
-			});
-		
-			socket.on('close', () => {
-				socket.destroy();
-				if (readableStreamCancel) {
-					return;
-				}
-				controller.close();
-			});
-		},
-	
-		pull(controller) {
-			// if ws can stop read if stream is full, we can implement backpressure
-			// https://streams.spec.whatwg.org/#example-rs-push-backpressure
-		},
-		cancel(reason) {
-			// 1. pipe WritableStream has error, this cancel will called, so ws handle server close into here
-			// 2. if readableStream is cancel, all controller.close/enqueue need skip,
-			// 3. but from testing controller.error still work even if readableStream is cancel
-			if (readableStreamCancel) {
-				return;
-			}
-			readableStreamCancel = true;
-			socket.destroy();
-		}
-	});
-
-	const onSocketCloses = new Promise((resolve, reject) => {
-		socket.on('close', (err) => {
-			if (err) {
-				reject(socket.errored);
-			} else {
-				resolve();
-			}
-		});
-
-		socket.on('error', (err) => {
-			reject(err);
-		});
-	});
-
-	return {
-		// A ReadableStream Object
-		readable: readableStream,
-	
-		// Contains functions to write to a TCP stream
-		writable: {
-			getWriter: () => {
-				return {
-					write: (data) => {
-						socket.write(data);
-					},
-					releaseLock: () => {
-						// console.log('Dummy writer.releaseLock()');
-					}
-				};
-			}
-		},
-
-		// Handles socket close
-		closed: onSocketCloses
-	};
-};
-
-platformAPI.newWebSocket = (url) => new WebSocket(url);
-
-platformAPI.associate = async (isIPv6) => {
-	const UDPSocket = createUDPSocket(isIPv6 ? 'udp6' : 'udp4');
-	return {
-		send: (datagram, offset, length, port, address, sendDoneCallback) => {
-			UDPSocket.send(datagram, offset, length, port, address, sendDoneCallback);
-		},
-		close: () => {
-			UDPSocket.close();
-		},
-		onmessage: (handler) => {
-			UDPSocket.on('message', handler);
-		},
-		onerror: (handler) => {
-			UDPSocket.on('error', handler);
-		}
-	};
 }
 
 async function loadModule() {
